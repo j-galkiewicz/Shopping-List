1b46e21ef8f66ae7b964ff8f52feada182f79dc6
J. Galkiewicz <46644880+j-galkiewicz@users.noreply.github.com>
2021-01-01 14:23:23 +0100

    clock intro

--- script.js
+++ script.js
@@ -89,3 +89,26 @@ list.addEventListener('click', function(e) {
 restoreFromLocalStorage();
 
 
+//Clock
+
+
+    const secondHand = document.querySelector('.second-hand');
+    const minHand = document.querySelector('.minute-hand');
+    const hourHand = document.querySelector('.hour-hand');
+
+    function setDate() {
+      const now = new Date();
+      //seconds hand
+      const seconds = now.getSeconds();
+      const secondsDegrees = ((seconds / 60) * 360) + 90;
+      secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+      //minutes hand
+      const minutes = now.getMinutes();
+      const minutesDegrees = ((minutes / 60) * 360) + 90;
+      minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+      //hours hand
+      const hours = now.getHours();
+      const hoursDegrees = ((hours / 12) * 360) + 90;
+      hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+      
+    }
